e96c0f849fa25a73cfcc097d0187c2bf1e5d9359
Tommaso Rinaldi <162431612+Tommaso2109@users.noreply.github.com>
2024-03-07 16:51:26 +0100

    esercizio

--- esercizio/Mini_Database/database.c
+++ esercizio/Mini_Database/database.c
@@ -106,7 +106,15 @@ typedef struct {
 void insert(Database * database, Persona * persona){
     
 }
-Persona* findByName(Database * database, char * name);
-Persona* findBySurname(Database * database, char * surname);
-Persona* findByAddress(Database * database, char * address);
-Persona* findByAge(Database * database, int age);
+Persona* findByName(Database * database, char * name){
+
+}
+Persona* findBySurname(Database * database, char * surname){
+
+}
+Persona* findByAddress(Database * database, char * address){
+
+}
+Persona* findByAge(Database * database, int age){
+    
+}
